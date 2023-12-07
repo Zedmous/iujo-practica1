d79a0351e9792a3cc595b9231aa18f802138af5d
Zedmous <ejnsilva29@gmail.com>
2023-12-07 03:32:11 -0400

    agregue la funcion suma

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const colors = require('colors');
 console.log("Bienvenidos a la primera practica")
-
-
 const uniforme = (x,k)=>{
 
     let prob= Number((x/k).toFixed(4))
     return {prob}
 }
+const suma =(a,b) =>{
 
+    return a+b
+}
 console.log(`La probabilidad calculada es: `+`${uniforme(3,6).prob}`.green)
